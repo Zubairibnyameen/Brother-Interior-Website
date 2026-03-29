2edbc2501351a414dad2437e47c73cc50ff4f4e6
Zubair Ibn Yameen <mozubairchoudhary@gmail.com>
2026-03-29 23:27:32 +0530

    ye pakka final fix

--- components/portfolio-gallery.tsx
+++ components/portfolio-gallery.tsx
@@ -85,7 +85,7 @@ export default function PortfolioGallery() {
         {/* Category Filter */}
         <div className="flex flex-wrap justify-center gap-4 mb-16">
           {categories.map(category => (
-            <Button
+            <button
               key={category}
               onClick={() => setActiveCategory(category)}
               className={`px-6 py-2 rounded-full font-medium transition-all ${
@@ -95,7 +95,7 @@ export default function PortfolioGallery() {
               }`}
             >
               {category.charAt(0).toUpperCase() + category.slice(1)}
-            </Button>
+            </button>
           ))}
         </div>
 
